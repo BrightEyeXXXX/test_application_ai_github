62faf083e889927248e98afec86b240affcb2380
miura takashi <miura.takashi0722@gmal.com>
2024-08-13 07:50:17 +0900

    Initial Commit

---
+++ test_application_ai_github/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test_application_ai_github
+//
+//  Created by 三浦隆 on 2024/08/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test_application_ai_github/test_application_ai_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_application_ai_githubApp.swift
+//  test_application_ai_github
+//
+//  Created by 三浦隆 on 2024/08/13.
+//
+
+import SwiftUI
+
+@main
+struct test_application_ai_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
